93c2edb09fc321d5b3e183e6f2aafce2519e0b68
Ildan Ibatullin <rustam162606@yandex.ru>
2025-01-03 22:55:56 +0300

    Создали файл utils.py который открывает файл в формате Json

---
+++ utils.py
@@ -0,0 +1,16 @@
+import json
+
+def load_transactions(file_path):
+    """
+    Загружает данные о транзакциях из JSON-файла.
+
+    :param file_path: Путь к JSON-файлу с данными о транзакциях.
+    :return: Список словарей с данными о транзакциях, если файл существует и содержит список,
+             иначе пустой список.
+    :raises FileNotFoundError: Если файл не найден.
+    :raises json.JSONDecodeError: Если файл содержит некорректные данные JSON.
+    """
+    with open(file_path, encoding='utf-8') as f:
+        data = json.load(f)  # Загружаем данные из файла в формате JSON
+    return data
+
